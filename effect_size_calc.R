97ff5381ca954f5fe8954b655bdb29334f32d154
Ahmed Y. Azzam <109034951+drazzam@users.noreply.github.com>
2023-03-30 21:29:31 +0200

    Create effect_size_calc.R

---
+++ effect_size_calc.R
@@ -0,0 +1,13 @@
+# -------------------- For Binary Data --------------------- #
+
+# Calculate the effect size (log risk ratio) and its standard error
+effect_sizes <- escalc(measure = "OR", ai = dataset$event1, n1i = dataset$total1, ci = dataset$event2, n2i = dataset$total2)
+
+# Add the effect sizes and their standard errors to the dataset
+dataset$effect_size <- effect_sizes$yi
+dataset$se_effect_size <- effect_sizes$vi
+
+# View the dataset with the added effect sizes and standard errors
+print(dataset)
+
+# -------------------- For Continous Data --------------------- #
