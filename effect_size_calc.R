0d59bf4d4f787dd825bb7bf163666ee79405b3d1
Ahmed Y. Azzam <109034951+drazzam@users.noreply.github.com>
2023-03-31 04:23:22 +0200

    Update effect_size_calc.R

--- effect_size_calc.R
+++ effect_size_calc.R
@@ -11,3 +11,13 @@ dataset$se_effect_size <- effect_sizes$vi
 print(dataset)
 
 # -------------------- For Continuous Data --------------------- #
+
+# Calculate the effect size (mean difference) and its standard error
+effect_sizes <- escalc(measure = "MD", m1i = dataset$mean1, sd1i = dataset$sd1, n1i = dataset$total1, m2i = dataset$mean2, sd2i = dataset$sd2, n2i = dataset$total2)
+
+# Add the effect sizes and their standard errors to the dataset
+dataset$effect_size <- effect_sizes$yi
+dataset$se_effect_size <- effect_sizes$vi
+
+# View the dataset with the added effect sizes and standard errors
+print(dataset)
