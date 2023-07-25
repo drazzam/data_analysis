5ca50073eea364703f21fccd2ae7ba9e0cd699da
Ahmed Y. Azzam <109034951+drazzam@users.noreply.github.com>
2023-07-25 17:54:59 +0300

    Update univariate_binary_regression.R

--- univariate_binary_regression.R
+++ univariate_binary_regression.R
@@ -1,12 +1,12 @@
 #fit logistic regression model
-model <- glm(Retreatement ~ age, family = "binomial", data = data)
+model <- glm(Retreatement ~ WEB_generation, family = "binomial", data = data)
 
 #view model summary
 summary(model)
 
 #extract coefficient and standard error for age
-coef <- summary(model)$coefficients["age", 1]
-se <- summary(model)$coefficients["age", 2]
+coef <- summary(model)$coefficients["WEB_generation", 1]
+se <- summary(model)$coefficients["WEB_generation", 2]
 
 #calculate odds ratio
 or <- exp(coef)
@@ -15,6 +15,6 @@ or <- exp(coef)
 ci <- exp(coef + qnorm(c(0.025, 0.975)) * se)
 
 #print results
-cat("P-value:", summary(model)$coefficients["age", 4], "\n")
+cat("P-value:", summary(model)$coefficients["WEB_generation", 4], "\n")
 cat("Odds ratio:", or, "\n")
 cat("95% confidence interval:", ci, "\n")
